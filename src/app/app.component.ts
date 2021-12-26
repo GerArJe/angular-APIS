8391ac1053eabd6df0d87d6772239c3a5e83f606
German Arevalo J <germanarevalojerez@gmail.com>
2021-12-25 20:55:14 -0500

    manejo de autenticacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 
-import { Product } from './models/product.model';
-
+import { AuthService } from './services/auth.service';
+import { UsersService } from './services/users.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   imgParent = '';
   showImg = true;
 
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   onLoaded(img: string) {
     console.log('log padre', img);
@@ -18,5 +22,19 @@ export class AppComponent {
 
   toggleImg() {
     this.showImg = !this.showImg;
+  }
+
+  createUser() {
+    this.usersService
+      .create({ name: 'Sebas', email: 'sebas@mail.com', password: '123456' })
+      .subscribe((data) => {
+        console.log(data);
+      });
+  }
+
+  login() {
+    this.authService.login('sebas@mail.com', '123456').subscribe((data) => {
+      console.log(data.access_token);
+    });
   }
 }
